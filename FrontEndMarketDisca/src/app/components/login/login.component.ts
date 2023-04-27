bd330c321f3c0a355e92fe978db0c6649c9c8aa5
JulianOrtega <julianortegacastillo@gmail.com>
2023-04-26 23:09:01 -0500

    alert en login y ajuste de cositas en profile

--- FrontEndMarketDisca/src/app/components/login/login.component.ts
+++ FrontEndMarketDisca/src/app/components/login/login.component.ts
@@ -14,6 +14,7 @@ export class LoginComponent implements OnInit {
   }
   email!: string;
   password!: string;
+  errors:boolean = false;
   iniciarSesionConGoogle() {
     // Lógica para iniciar sesión con Google
   }
@@ -49,9 +50,11 @@ export class LoginComponent implements OnInit {
         );
 
         this.router.navigate(['profile']);
+        this.errors=false;
       },
       error: (err) => {
-        console.log(err)
+        console.log(err);
+        this.errors=true;
       }
     });
   }
